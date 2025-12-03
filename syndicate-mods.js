245dbddcda4916d840d7c96e2260fcff561dc7f0
Dave <dawid.komperda1@gmail.com>
2025-12-03 01:08:31 +0100

    Fix mod slugs: change dashes to underscores for API consistency

--- syndicate-mods.js
+++ syndicate-mods.js
@@ -355,353 +355,353 @@ const SYNDICATE_MODS = {
   // The Perrin Sequence - Right alignment
   'The Perrin Sequence': [
     // Weapon Augments
-    'toxic-sequence',
-    'deadly-sequence',
-    'voltage-sequence',
-    'sequence-burn',
+    'toxic_sequence',
+    'deadly_sequence',
+    'voltage_sequence',
+    'sequence_burn',
     
     // Warframe Augments - Banshee
-    'sonic-fracture',
+    'sonic_fracture',
     'resonance',
-    'savage-silence',
-    'resonating-quake',
+    'savage_silence',
+    'resonating_quake',
     
     // Warframe Augments - Caliban
-    'razor-mortar',
+    'razor_mortar',
     
     // Warframe Augments - Chroma
     'afterburn',
-    'everlasting-ward',
-    'vexing-retaliation',
-    'guardian-armor',
-    'guided-effigy',
+    'everlasting_ward',
+    'vexing_retaliation',
+    'guardian_armor',
+    'guided_effigy',
     
     // Warframe Augments - Dagath
-    'spectral-spirit',
+    'spectral_spirit',
     
     // Warframe Augments - Gauss
-    'mach-crash',
-    'thermal-transfer',
+    'mach_crash',
+    'thermal_transfer',
     
     // Warframe Augments - Gyre
-    'conductive-sphere',
-    'coil-recharge',
-    'cathode-current',
-    'reverse-rotorswell',
+    'conductive_sphere',
+    'coil_recharge',
+    'cathode_current',
+    'reverse_rotorswell',
     
     // Warframe Augments - Hildryn
-    'balefire-surge',
-    'blazing-pillage',
-    'aegis-gale',
+    'balefire_surge',
+    'blazing_pillage',
+    'aegis_gale',
     
     // Warframe Augments - Inaros
-    'desiccations-curse',
-    'elemental-sandstorm',
-    'negation-armor',
+    'desiccations_curse',
+    'elemental_sandstorm',
+    'negation_armor',
     
     // Warframe Augments - Ivara
-    'empowered-quiver',
-    'piercing-navigator',
+    'empowered_quiver',
+    'piercing_navigator',
     'infiltrate',
-    'concentrated-arrow',
+    'concentrated_arrow',
     
     // Warframe Augments - Mag
-    'greedy-pull',
-    'magnetized-discharge',
-    'counter-pulse',
-    'fracturing-crush',
+    'greedy_pull',
+    'magnetized_discharge',
+    'counter_pulse',
+    'fracturing_crush',
     
     // Warframe Augments - Nekros
-    'soul-survivor',
-    'creeping-terrify',
+    'soul_survivor',
+    'creeping_terrify',
     'despoil',
-    'shield-of-shadows',
+    'shield_of_shadows',
     
     // Warframe Augments - Nidus
-    'teeming-virulence',
-    'larva-burst',
-    'parasitic-vitality',
+    'teeming_virulence',
+    'larva_burst',
+    'parasitic_vitality',
     'insatiable',
-    'abundant-mutation',
+    'abundant_mutation',
     
     // Warframe Augments - Protea
-    'repair-dispensary',
-    'temporal-artillery',
-    'temporal-erosion',
+    'repair_dispensary',
+    'temporal_artillery',
+    'temporal_erosion',
     
     // Warframe Augments - Revenant
-    'thrall-pact',
-    'mesmer-shield',
-    'blinding-reave',
+    'thrall_pact',
+    'mesmer_shield',
+    'blinding_reave',
     
     // Warframe Augments - Rhino
-    'ironclad-charge',
-    'iron-shrapnel',
-    'piercing-roar',
-    'reinforcing-stomp',
+    'ironclad_charge',
+    'iron_shrapnel',
+    'piercing_roar',
+    'reinforcing_stomp',
     
     // Warframe Augments - Trinity
-    'pool-of-life',
-    'vampire-leech',
-    'abating-link',
-    'champions-blessing',
+    'pool_of_life',
+    'vampire_leech',
+    'abating_link',
+    'champions_blessing',
     
     // Warframe Augments - Valkyr
-    'swing-line',
-    'eternal-war',
-    'prolonged-paralysis',
-    'hysterical-assault',
+    'swing_line',
+    'eternal_war',
+    'prolonged_paralysis',
+    'hysterical_assault',
     'enraged',
     
     // Warframe Augments - Vauban
-    'tesla-bank',
-    'repelling-bastille',
-    'photon-repeater',
+    'tesla_bank',
+    'repelling_bastille',
+    'photon_repeater',
     
     // Warframe Augments - Sevagoth
-    'shadow-haze',
-    'dark-propagation',
+    'shadow_haze',
+    'dark_propagation',
     
     // Syndicate Weapons
-    'secura-dual-cestra',
-    'secura-penta',
-    'secura-lecta',
+    'secura_dual_cestra',
+    'secura_penta',
+    'secura_lecta',
   ],
   
   // Red Veil - Left alignment
   'Red Veil': [
     // Weapon Augments
-    'gleaming-blight',
-    'eroding-blight',
-    'stockpiled-blight',
-    'toxic-blight',
+    'gleaming_blight',
+    'eroding_blight',
+    'stockpiled_blight',
+    'toxic_blight',
     
     // Warframe Augments - Ash
-    'seeking-shuriken',
-    'smoke-shadow',
-    'teleport-rush',
-    'rising-storm',
+    'seeking_shuriken',
+    'smoke_shadow',
+    'teleport_rush',
+    'rising_storm',
     
     // Warframe Augments - Atlas
-    'path-of-statues',
-    'tectonic-fracture',
-    'ore-gaze',
-    'titanic-rumbler',
-    'rubble-heap',
+    'path_of_statues',
+    'tectonic_fracture',
+    'ore_gaze',
+    'titanic_rumbler',
+    'rubble_heap',
     
     // Warframe Augments - Citrine
-    'prismatic-companion',
+    'prismatic_companion',
     'recrystalize',
     
     // Warframe Augments - Dagath
-    'spectral-spirit',
+    'spectral_spirit',
     
     // Warframe Augments - Ember
-    'fireball-frenzy',
-    'immolated-radiance',
-    'healing-flame',
+    'fireball_frenzy',
+    'immolated_radiance',
+    'healing_flame',
     'exothermic',
     
     // Warframe Augments - Excalibur Umbra
-    'warriors-rest',
+    'warriors_rest',
     
     // Warframe Augments - Garuda
-    'dread-ward',
-    'blood-forge',
-    'blending-talons',
+    'dread_ward',
+    'blood_forge',
+    'blending_talons',
     
     // Warframe Augments - Grendel
     'gourmand',
-    'hearty-nourishment',
+    'hearty_nourishment',
     'catapult',
     'gastro',
     
     // Warframe Augments - Harrow
     'tribunal',
-    'warding-thurible',
-    'lasting-covenant',
+    'warding_thurible',
+    'lasting_covenant',
     
     // Warframe Augments - Jade
-    'jades-judgment',
+    'jades_judgment',
     
     // Warframe Augments - Khora
-    'accumulating-whipclaw',
-    'venari-bodyguard',
-    'pilfering-strangledome',
+    'accumulating_whipclaw',
+    'venari_bodyguard',
+    'pilfering_strangledome',
     
     // Warframe Augments - Lavos
-    'swift-bite',
-    'valence-formation',
-    'lingering-transmutation',
+    'swift_bite',
+    'valence_formation',
+    'lingering_transmutation',
     
     // Warframe Augments - Loki
-    'savior-decoy',
-    'damage-decoy',
-    'hushed-invisibility',
-    'safeguard-switch',
-    'irradiating-disarm',
+    'savior_decoy',
+    'damage_decoy',
+    'hushed_invisibility',
+    'safeguard_switch',
+    'irradiating_disarm',
     
     // Warframe Augments - Mesa
-    'ballistic-bullseye',
-    'staggering-shield',
-    'muzzle-flash',
-    'mesas-waltz',
+    'ballistic_bullseye',
+    'staggering_shield',
+    'muzzle_flash',
+    'mesas_waltz',
     
     // Warframe Augments - Nekros
-    'soul-survivor',
-    'creeping-terrify',
+    'soul_survivor',
+    'creeping_terrify',
     'despoil',
-    'shield-of-shadows',
+    'shield_of_shadows',
     
     // Warframe Augments - Saryn
-    'venom-dose',
-    'revealing-spores',
-    'regenerative-molt',
-    'contagion-cloud',
+    'venom_dose',
+    'revealing_spores',
+    'regenerative_molt',
+    'contagion_cloud',
     
     // Warframe Augments - Titania
-    'spellbound-harvest',
-    'beguiling-lantern',
-    'razorwing-blitz',
-    'ironclad-flight',
+    'spellbound_harvest',
+    'beguiling_lantern',
+    'razorwing_blitz',
+    'ironclad_flight',
     
     // Warframe Augments - Volt
-    'shock-trooper',
-    'shocking-speed',
-    'transistor-shield',
+    'shock_trooper',
+    'shocking_speed',
+    'transistor_shield',
     'capacitance',
     
     // Warframe Augments - Voruna
-    'prey-of-dynar',
-    'ulfruns-endurance',
+    'prey_of_dynar',
+    'ulfruns_endurance',
     
     // Warframe Augments - Zephyr
-    'target-fixation',
-    'airburst-rounds',
-    'jet-stream',
-    'funnel-clouds',
-    'anchored-glide',
+    'target_fixation',
+    'airburst_rounds',
+    'jet_stream',
+    'funnel_clouds',
+    'anchored_glide',
     
     // Syndicate Weapons
-    'rakta-ballistica',
-    'rakta-cernos',
-    'rakta-dark-dagger',
+    'rakta_ballistica',
+    'rakta_cernos',
+    'rakta_dark_dagger',
   ],
   
   // New Loka - Left alignment
   'New Loka': [
     // Weapon Augments
-    'winds-of-purity',
-    'disarming-purity',
-    'bright-purity',
-    'lasting-purity',
+    'winds_of_purity',
+    'disarming_purity',
+    'bright_purity',
+    'lasting_purity',
     
     // Warframe Augments - Baruuk
-    'elusive-retribution',
-    'endless-lullaby',
-    'reactive-storm',
+    'elusive_retribution',
+    'endless_lullaby',
+    'reactive_storm',
     
     // Warframe Augments - Equinox
     'duality',
-    'calm-frenzy',
-    'peaceful-provocation',
-    'energy-transfer',
+    'calm_frenzy',
+    'peaceful_provocation',
+    'energy_transfer',
     
     // Warframe Augments - Gara
-    'shattered-storm',
-    'mending-splinters',
+    'shattered_storm',
+    'mending_splinters',
     'spectrosiphon',
     
     // Warframe Augments - Hydroid
-    'viral-tempest',
-    'tidal-impunity',
-    'rousing-plunder',
-    'pilfering-swarm',
+    'viral_tempest',
+    'tidal_impunity',
+    'rousing_plunder',
+    'pilfering_swarm',
     
     // Warframe Augments - Koumei
-    'omikujis-fortune',
+    'omikujis_fortune',
     
     // Warframe Augments - Kullervo
-    'volatile-recompense',
-    'wrath-of-ukko',
+    'volatile_recompense',
+    'wrath_of_ukko',
     
     // Warframe Augments - Lavos
-    'swift-bite',
-    'valence-formation',
-    'lingering-transmutation',
+    'swift_bite',
+    'valence_formation',
+    'lingering_transmutation',
     
     // Warframe Augments - Mag
-    'greedy-pull',
-    'magnetized-discharge',
-    'counter-pulse',
-    'fracturing-crush',
+    'greedy_pull',
+    'magnetized_discharge',
+    'counter_pulse',
+    'fracturing_crush',
     
     // Warframe Augments - Nyx
-    'mind-freak',
-    'pacifying-bolts',
-    'chaos-sphere',
+    'mind_freak',
+    'pacifying_bolts',
+    'chaos_sphere',
     'assimilate',
     
     // Warframe Augments - Oberon
-    'smite-infusion',
-    'hallowed-eruption',
-    'phoenix-renewal',
-    'hallowed-reckoning',
+    'smite_infusion',
+    'hallowed_eruption',
+    'phoenix_renewal',
+    'hallowed_reckoning',
     
     // Warframe Augments - Octavia
-    'partitioned-mallet',
+    'partitioned_mallet',
     'conductor',
     
     // Warframe Augments - Titania
-    'spellbound-harvest',
-    'beguiling-lantern',
-    'razorwing-blitz',
-    'ironclad-flight',
+    'spellbound_harvest',
+    'beguiling_lantern',
+    'razorwing_blitz',
+    'ironclad_flight',
     
     // Warframe Augments - Styanax
-    'axios-javelineers',
-    'tharros-lethality',
-    'intrepid-stand',
+    'axios_javelineers',
+    'tharros_lethality',
+    'intrepid_stand',
     
     // Warframe Augments - Trinity
-    'pool-of-life',
-    'vampire-leech',
-    'abating-link',
-    'champions-blessing',
+    'pool_of_life',
+    'vampire_leech',
+    'abating_link',
+    'champions_blessing',
     
     // Warframe Augments - Valkyr
-    'swing-line',
-    'eternal-war',
-    'prolonged-paralysis',
-    'hysterical-assault',
+    'swing_line',
+    'eternal_war',
+    'prolonged_paralysis',
+    'hysterical_assault',
     'enraged',
     
     // Warframe Augments - Wisp
-    'fused-reservoir',
-    'critical-surge',
-    'cataclysmic-gate',
+    'fused_reservoir',
+    'critical_surge',
+    'cataclysmic_gate',
     
     // Warframe Augments - Zephyr
-    'target-fixation',
-    'airburst-rounds',
-    'jet-stream',
-    'funnel-clouds',
-    'anchored-glide',
+    'target_fixation',
+    'airburst_rounds',
+    'jet_stream',
+    'funnel_clouds',
+    'anchored_glide',
     
     // Warframe Augments - Wukong
-    'celestial-stomp',
-    'enveloping-cloud',
-    'primal-rage',
+    'celestial_stomp',
+    'enveloping_cloud',
+    'primal_rage',
     
     // Warframe Augments - Yareli
-    'merulina-guardian',
-    'loyal-merulina',
-    'surging-blades',
+    'merulina_guardian',
+    'loyal_merulina',
+    'surging_blades',
     
     // Syndicate Weapons
-    'sancti-castanas',
-    'sancti-tigris',
-    'sancti-magistar',
+    'sancti_castanas',
+    'sancti_tigris',
+    'sancti_magistar',
   ],
 };
 
